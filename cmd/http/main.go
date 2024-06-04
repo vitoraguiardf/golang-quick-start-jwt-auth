b13f4abb3f8c17cbe7921825b6595bee945c7822
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-06-03 21:52:13 -0300

    env file

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/vitoraguiardf/golang-quick-start-jwt-auth/internal/core/services"
 	"github.com/vitoraguiardf/golang-quick-start-jwt-auth/internal/handlers/usershdl"
 	"github.com/vitoraguiardf/golang-quick-start-jwt-auth/internal/repositories/usersrepo"
 )
 
 func main() {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
+
 	router := gin.Default()
 
 	userRepository := usersrepo.NewRepository()
