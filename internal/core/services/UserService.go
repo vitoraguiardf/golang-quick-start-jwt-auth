c17797f64011d834ab69f9cde6a2806be97e4538
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-06-05 21:50:35 -0300

    refactor user service

--- internal/core/services/UserService.go
+++ internal/core/services/UserService.go
@@ -5,18 +5,18 @@ import (
 	"github.com/vitoraguiardf/golang-quick-start-jwt-auth/internal/core/ports"
 )
 
-type service struct {
+type userService struct {
 	repository ports.UserRepository
 }
 
-func NewUserService(userRepository ports.UserRepository) *service {
-	return &service{
+func NewUserService(userRepository ports.UserRepository) *userService {
+	return &userService{
 		repository: userRepository,
 	}
 }
 
-func (svc *service) Get(id uint) (domain.User, error) {
-	user, err := svc.repository.Get(id)
+func (service *userService) Get(id uint) (domain.User, error) {
+	user, err := service.repository.Get(id)
 	if err != nil {
 		return domain.User{}, err // get model from repository has failed
 	}
