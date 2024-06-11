7e7fa19b1afcd4eafdc32b0cbb12501ee4f6f709
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-06-11 11:18:29 -0300

    fix sqlite test connection

--- internal/repositories/persistence/sqlite_test.go
+++ internal/repositories/persistence/sqlite_test.go
@@ -1,10 +1,12 @@
-package persistence
+package persistence_test
 
 import (
 	"testing"
+
+	"github.com/vitoraguiardf/golang-quick-start-jwt-auth/internal/repositories/persistence"
 )
 
 func TestConnection(t *testing.T) {
 	t.Setenv("DB_DATABASE", "test.db")
-	NewSqliteDB().InitDB()
+	persistence.NewSqliteDB().InitDB()
 }
