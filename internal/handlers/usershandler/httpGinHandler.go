97fc2fa17fde48d7b86a11e27a3e897da3b1ea6c
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-06-10 21:11:31 -0300

    fix get router

--- internal/handlers/usershandler/httpGinHandler.go
+++ internal/handlers/usershandler/httpGinHandler.go
@@ -24,14 +24,24 @@ func (handler *HTTPGinHandler) RegistryRoutes(router *gin.Engine) {
 }
 
 func (handler *HTTPGinHandler) Get(c *gin.Context) {
-	id, err := strconv.Atoi(c.Param("id"))
-	if err != nil {
-		c.AbortWithStatusJSON(500, gin.H{"error": err.Error()})
+	var id uint
+	if param_id := c.Param("id"); len(param_id) > 0 {
+		if key, err := strconv.Atoi(param_id); err == nil {
+			id = uint(key)
+		} else {
+			c.AbortWithStatusJSON(500, gin.H{"error": err.Error()})
+			return
+		}
 	}
-	user, err := handler.service.Get(uint(id))
-	if err != nil {
-		c.AbortWithStatusJSON(500, gin.H{"error": err.Error()})
-		return
+
+	if id != 0 {
+		if user, err := handler.service.Get(uint(id)); err != nil {
+			c.AbortWithStatusJSON(500, gin.H{"error": err.Error()})
+			return
+		} else {
+			c.JSON(200, user)
+			return
+		}
 	}
-	c.JSON(200, user)
+	c.JSON(200, gin.H{"message": "listing all users"})
 }
